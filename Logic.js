83d9b77fb34467a9ccbbf6fb899526c10f7c95b5
WaelKendouli <kendouliwael@gmail.com>
2026-09-03 17:40:44 +0200

    Adding UI components

---
+++ Logic.js
@@ -0,0 +1,25 @@
+const UI = {
+    // ✅ Input
+       birthDateInput : document.getElementById("birthDate"),
+
+      // ✅ Buttons
+       btnCalc : document.getElementById("btnCalc"),
+       btnToday : document.getElementById("btnToday"),
+       btnClear : document.getElementById("btnClear"),
+
+      // ✅ Output numbers
+       outYears : document.getElementById("outYears"),
+       outMonths : document.getElementById("outMonths"),
+       outDays : document.getElementById("outDays"),
+
+      // ✅ Output text lines
+       nextBirthdayLine : document.getElementById("nextBirthdayLine"),
+       extraLine : document.getElementById("extraLine"),
+
+      // ✅ Header/status chips
+       todayChip : document.getElementById("todayChip"),
+       statusChip : document.getElementById("statusChip"),
+
+      // ✅ Message box (neutral/success/error)
+       msgBox : document.getElementById("msgBox")
+}
